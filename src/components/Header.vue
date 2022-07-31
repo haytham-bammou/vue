cba99faa1e2670a40a50c42a32b3cf3f081aebcc
haytham-bammou <Haythambm86@gmail.com>
2022-07-31 23:54:33 +0100

    deploying vue front end

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @click="$emit('show-AddTask')" :text="showAddTask ?  'close' : 'Add Task'" :color="showAddTask ?  'red' : 'green'"/>
+    </header>
+</template>
+
+<script>
+    import Button from './Button.vue'; 
+    export default {
+        name: "header",
+        props : {
+            title : {
+                type : String ,
+                default : "default "
+            },
+            showAddTask : Boolean,
+        },
+        components : {
+            Button
+        },
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
